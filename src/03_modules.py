96009ed1c517f0f9b2f066a07a5c77e00b9c20bb
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-12-02 20:22:15 -0800

    finished half the project

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,30 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+if len(sys.argv) >= 1:
+    for arg in sys.argv:
+        print(arg)
+else:
+    print('no arguments given')
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print('The os platform is ' + sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print('Python version ' + sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print('the current process ID' + str(os.getpid()))
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print('current working directory ' + os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print('machine login name ' + os.getlogin()) 
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,14 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [num+1 for num in range(5)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [num*num*num for num in range(10)]
 
 print(y)
 
@@ -26,16 +26,16 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [a.upper() for a in a]
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
-
+x = "1,2,3,4,5,6".split(',')
+#print('printing x: %s' % x)
 # What do you need between the square brackets to make it work?
-y = []
+y = [z for z in x if int(z) % 2==0]
 
 print(y)
